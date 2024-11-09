c961b197b2d1c4bbb96cdf3736d45b0f06c90d6b
Eduardo da Veiga Spiandorello <eduardo.spiandorello@isaac.com.br>
2024-11-09 13:31:37 -0300

    remove unsed var

--- src/lib/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAbv8k8HsZn72p1KOQQnMJBd7a3NTgHsd4',
-  authDomain: 'property-flow-36eb1.firebaseapp.com',
-  projectId: 'property-flow-36eb1',
-  storageBucket: 'property-flow-36eb1.firebasestorage.app',
-  messagingSenderId: '155500109653',
-  appId: '1:155500109653:web:094901a909593d4426d9dc',
-  measurementId: 'G-WZFRCSLQ8S',
-}
